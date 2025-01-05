bluca/mkosi-fedora

commit bbd2e37caab1e40a0b6c44203323b31343adcfcc

Update to 24 upstream release
- Resolves: rhbz#2292334

Upstream tag: v24
Upstream commit: be39f33d

Commit authored by Packit automation (https://packit.dev/)

mkosi.spec
@@ -1,5 +1,5 @@
 Name:           mkosi
-Version:        23.1
+Version:        24
 Release:        %autorelease
 Summary:        Create bespoke OS images
 
@@ -126,6 +126,8 @@ tools/make-man-page.sh
 mkdir -p %{buildroot}%{_mandir}/man1
 ln -s -t %{buildroot}%{_mandir}/man1/ \
          ../../../..%{python3_sitelib}/mkosi/resources/mkosi.1
+ln -s -t %{buildroot}%{_mandir}/man1/ \
+         ../../../..%{python3_sitelib}/mkosi/resources/mkosi-initrd.1
 
 # Install the kernel-install plugin
 install -Dt %{buildroot}%{_prefix}/lib/kernel/install.d/ \
@@ -140,6 +142,8 @@ mkdir -p %{buildroot}%{_sysconfdir}/mkosi-initrd
 %_mandir/man1/mkosi.1*
 
 %files initrd
+%_bindir/mkosi-initrd
+%_mandir/man1/mkosi-initrd.1*
 %_prefix/lib/kernel/install.d/50-mkosi.install
 %ghost %dir %{_prefix}/lib/mkosi-initrd
 %ghost %dir %{_sysconfdir}/mkosi-initrd

.gitignore
@@ -21,3 +21,4 @@
 /mkosi-21.tar.gz
 /mkosi-22.tar.gz
 /mkosi-23.1.tar.gz
+/mkosi-24.tar.gz

sources
@@ -1,1 +1,1 @@
-SHA512 (mkosi-23.1.tar.gz) = 27004701e93036efc8c1f14981006418672a23fc8f705cb2dbd1b4fca8ced4b1eae94f4eecb07988630a72b70516b947ff28a7686d49a0daa2dd2dd8d83578b4
+SHA512 (mkosi-24.tar.gz) = 20d72eb62712d4a64ab32a6b145c570bcbd07d1dd02777e23fb3bba9a486a42fdf551f5346d5b2a525c20c71570976f3b4f63b07154c1bd7bbf8820eb4e72a15

commit 1ed012dd35198dbdd3c3d7faa32b2506898b46fc

Fix symlink to mkosi-initrd.1 man page
Fixes a bug introduced by bbd2e37caab1e40a0b6c44203323b31343adcfcc.

mkosi.spec
@@ -128,7 +128,7 @@ mkdir -p %{buildroot}%{_mandir}/man1
 ln -s -t %{buildroot}%{_mandir}/man1/ \
          ../../../..%{python3_sitelib}/mkosi/resources/mkosi.1
 ln -s -t %{buildroot}%{_mandir}/man1/ \
-         ../../../..%{python3_sitelib}/mkosi/resources/mkosi-initrd.1
+         ../../../..%{python3_sitelib}/mkosi/initrd/resources/mkosi-initrd.1
 
 # Install the kernel-install plugin
 install -Dt %{buildroot}%{_prefix}/lib/kernel/install.d/ \